65bb89e02a02b870cfc98ae22b9eabba870949d6
khangltt2003 <khangltt2003@gmail.com>
2024-10-16 22:06:17 -0500

    fix resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -5,7 +5,11 @@ const Resume = () => {
   return (
     <div className="w-full h-full gradient-background text-white p-8 flex flex-col items-center">
       <h2 className="text-3xl font-semibold mb-6">My Resume</h2>
-      <iframe src={resume} title="Resume" className="w-[1200px] h-[10000px] mb-4 border-2 border-gray-300 rounded-md shadow-lg"></iframe>
+      <iframe
+        src={resume}
+        title="Resume"
+        className="w-[90%] md-[80%]  lg-[1200] h-[10000px] mb-4 border-2 border-gray-300 rounded-md shadow-lg"
+      ></iframe>
     </div>
   );
 };
